6075d68375105153eebfed7006fd32f80e7dee34
Dennis T Kaplan <gruessle@gmail.com>
2012-07-12 00:27:33 -0700

    styling

--- assets/js/module.js
+++ assets/js/module.js
@@ -3,7 +3,7 @@
 angular.module('Module', ['Filters', 'Services']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/lobby', {templateUrl: '/assests/html/lobby.html'}).
-      when('/copyright', {templateUrl: '/assests/html/copyright.html'}).
+      when('/lobby', {templateUrl: '/assets/html/lobby.html'}).
+      when('/copyright', {templateUrl: '/assets/html/copyright.html'}).
       otherwise({redirectTo: '/lobby'});
 }]);
