2a6ba781485850b3977ca84a0fbed04d09f503dd
Dennis T Kaplan <gruessle@gmail.com>
2012-07-12 00:59:03 -0700

    styling

--- assets/js/module.js
+++ assets/js/module.js
@@ -7,3 +7,33 @@ angular.module('Module', ['Filters', 'Services']).
       when('/copyright', {templateUrl: '/assets/html/copyright.html'}).
       otherwise({redirectTo: '/lobby'});
 }]);
+
+angular.module('Module', [], function($routeProvider, $locationProvider) {
+    $routeProvider.when('/lobby', {
+    templateUrl: '/assets/html/lobby.html',
+    controller: BookCntl
+    });
+    $routeProvider.when('/copyright', {
+    templateUrl: '/assets/html/copyright.html',
+    controller: ChapterCntl
+    });
+     
+    // configure html5 to get links working on jsfiddle
+    $locationProvider.html5Mode(true);
+    });
+     
+    function MainCntl($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+    }
+     
+    function BookCntl($scope, $routeParams) {
+    $scope.name = "BookCntl";
+    $scope.params = $routeParams;
+    }
+     
+    function ChapterCntl($scope, $routeParams) {
+    $scope.name = "ChapterCntl";
+    $scope.params = $routeParams;
+    }
