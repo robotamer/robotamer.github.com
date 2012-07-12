f89217a211668e60813161b09476da88e0f03aa5
Dennis T Kaplan <gruessle@gmail.com>
2012-07-12 03:05:10 -0700

    styling

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,39 +1,34 @@
-'use strict';
 
-angular.module('Module', []).
-  config(['$routeProvider', function($routeProvider, $locationProvider) {
-  $routeProvider.
-      when('/lobby', {templateUrl: '/assets/html/lobby.html', controller: ChapterCntl}).
-      when('/copyright', {templateUrl: '/assets/html/copyright.html', controller: BookCntl}).
-      otherwise({redirectTo: '/lobby'});
-}]);
 
-angular.module('Module2', [], function($routeProvider, $locationProvider) {
-    $routeProvider.when('/lobby', {
-    templateUrl: '/assets/html/lobby.html',
-    controller: BookCntl
+angular.module('Module', [], function($routeProvider, $locationProvider) {
+
+    $routeProvider.when('', {
+	templateUrl: '/assets/html/lobby.html', 
+	controller: LobbyCntl
     });
-    $routeProvider.when('/copyright', {
-    templateUrl: '/assets/html/copyright.html',
-    controller: ChapterCntl
+
+    $routeProvider.when('/Copyright', {
+	templateUrl: '/assets/html/copyright.html', 
+	controller: CopyrightCntl
     });
-     
-    // configure html5 to get links working on jsfiddle
-    $locationProvider.html5Mode(true);
-    });
-     
-    function MainCntl($scope, $route, $routeParams, $location) {
+
+    $routeProvider.otherwise({redirectTo: ''});
+
+});
+
+function MainCntl($scope, $route, $routeParams, $location) {
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
-    }
+}
      
-    function BookCntl($scope, $routeParams) {
-    $scope.name = "BookCntl";
-    $scope.params = $routeParams;
-    }
+function LobbyCntl($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+}
      
-    function ChapterCntl($scope, $routeParams) {
-    $scope.name = "ChapterCntl";
+function CopyrightCntl($scope, $routeParams) {
+    $scope.name = "CopyrightCntl";
     $scope.params = $routeParams;
-    }
+}
